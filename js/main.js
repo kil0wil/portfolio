8af6e5d5f39662469ad8acdff64c687da0c435ba
DWIGHT WILLIAMS <KIL0WILL@AOL.COM>
2020-08-19 01:26:42 -0700

    update to social media

--- js/main.js
+++ js/main.js
@@ -14,13 +14,13 @@ function homePage() {
 }
 //about me page
 function aboutMe() {
-    document.getElementById("content").innerHTML = "<div class='container'><section class='column2'><h2>Here is my story</h2>I am born and raised in Long Beach,ca. I am marraid and a father of two adventerous wild boys.  I <b>started</b> coding because I was curious on how things worked.  I taught myslef HTML, then CSS, Javascript, and jquary.  I ended up getting side track from my journey and started teaching myself python and C\#.  I even converted to linux started playing around with different flavors ran a LAMP stack, got even more distracted with linux kali.  Now <b> 4 years </b> later my skills are shareper, and I have a very diverse knowldege on dev ops<article><p></p></article></section><section class='column1'><h2>My reading list</h2></section></div>";
+    document.getElementById("content").innerHTML = "<div class='container'><section class='column2'><h2>Here is my story</h2>I am born and raised in Long Beach,ca. I am marraid and a father of two adventerous wild boys.  I <b>started</b> coding because I was curious on how things worked.  I taught myslef HTML, then CSS, Javascript, and jquary.  I ended up getting side track from my journey and started teaching myself python and C\#.  I even converted to linux started playing around with different flavors ran a LAMP stack, got even more distracted with linux kali.  Now <b> 4 years </b> later my skills are shareper, and I have a very diverse knowldege on dev ops<article><p></p></article></section><section class='column1'><h2>My reading list</h2><ul><li>HTML & CSS</li><li>JAVASCRIPT & JQUERY</li><li>Eloquent Javascript</li><li>Above the Fold</li><li>Learning PHP</li><li>Data Visualization with Javascript</li><li>learning node</li><li>the SEO battlefield</li></ul></section></div>";
     document.getElementById("contentTitle").innerHTML = "About Me";
     document.getElementById("contentQuote").innerHTML = randomQuotes();
 }
 //skills page
 function mySkill() {
-  document.getElementById("content").innerHTML = "skills stuff";
+    document.getElementById("content").innerHTML = "<iframe src='http://www.puddanz.com'style='height:200px;width:300px;'title='description'><br /><iframe src='http://www.thrive-branding.com'style='height:200px;width:300px;'title='description'>";
   document.getElementById("contentTitle").innerHTML = "skills";
   document.getElementById("contentQuote").innerHTML = randomQuotes();
 }
